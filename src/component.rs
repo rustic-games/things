ca40362e3a1268eb97afc177f4c4f47dfeabcace
Jean Mertz <jean@mertz.fm>
2018-11-11 13:35:05 +0100

    feat(component): auto implement Component trait
    `Component` is automatically implemented for any object that has both a
    static lifetime, and implements `Debug`.

--- src/component.rs
+++ src/component.rs
@@ -1,3 +1,22 @@
+use core::fmt::Debug;
+
 /// The `Component` trait is a marker trait that allows any object implementing
 /// it to be stored in a component store.
-pub trait Component: 'static + core::fmt::Debug {}
+pub trait Component: 'static + Debug {}
+
+/// `Component` is automatically implemented for any object that has both a
+/// static lifetime, and implements `Debug`.
+impl<T> Component for T where T: 'static + Debug {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_implicit_component_trait() {
+        #[derive(Debug)]
+        struct C;
+
+        let _: Box<Component> = Box::new(C);
+    }
+}
