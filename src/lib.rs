80704a9cbba14007db46affe23b586197aa24c54
Jean Mertz <jean@mertz.fm>
2018-12-19 19:45:04 +0100

    docs: initial top-level crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//! Things is a safe, fast and secure Entity–Component–System library.
+//!
+//! Entity–Component–System (ECS) is an architectural pattern that is mostly
+//! used in game development. ECS follows the composition over inheritance
+//! principle that allows greater flexibility in defining entities where every
+//! object in a game's scene is an entity (e.g. enemies, bullets, vehicles,
+//! etc.). Every Entity consists of one or more components which add behaviour
+//! or functionality. Therefore, the behaviour of an entity can be changed at
+//! runtime by adding or removing components. This eliminates the ambiguity
+//! problems of deep and wide inheritance hierarchies that are difficult to
+//! understand, maintain and extend. Common ECS approaches are highly compatible
+//! and often combined with data-oriented design techniques.
+//!
+//! see: https://en.wikipedia.org/wiki/Entity–component–system
+
 #![cfg_attr(feature = "doc", feature(external_doc))]
 
 mod component;
