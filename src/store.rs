c41be8c7e4ca4a07894466a53142250f33661e89
Jean Mertz <jean@mertz.fm>
2018-12-19 19:46:08 +0100

    feat(store): introduce ComponentStore trait
    This is a generic store trait that can be downcasted to a concrete
    storage type (provided by the `downcast-rs` crate).
    
    For now, it always downcasts to the `DefaultStore` store type.

--- src/store.rs
+++ src/store.rs
@@ -1,5 +1,6 @@
 use crate::Component;
 use core::fmt::Debug;
+use downcast_rs::{impl_downcast, Downcast};
 
 /// `Store` must be implemented by any object that wants to store
 /// [`Component`]s.
@@ -28,33 +29,73 @@ pub trait Store: Sized + Default + Debug {
     fn push(&mut self, component: Self::Item) -> usize;
 }
 
+pub trait ComponentStore: Downcast {}
+impl_downcast!(ComponentStore);
+
+impl ComponentStore {
+    pub fn as_store<C: Component>(&self) -> Option<&DefaultStore<C>> {
+        self.downcast_ref::<DefaultStore<C>>()
+    }
+
+    pub fn as_store_mut<C: Component>(&mut self) -> Option<&mut DefaultStore<C>> {
+        self.downcast_mut::<DefaultStore<C>>()
+    }
+}
+
+#[derive(Debug)]
+pub struct DefaultStore<C: Component> {
+    data: Vec<C>,
+}
+
+impl<C: Component> ComponentStore for DefaultStore<C> {}
+
+impl<C: Component> Default for DefaultStore<C> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<C: Component> Store for DefaultStore<C> {
+    type Item = C;
+
+    fn new() -> Self {
+        Self { data: Vec::new() }
+    }
+
+    fn push(&mut self, component: Self::Item) -> usize {
+        self.data.push(component);
+        self.data.len()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use core::marker::PhantomData;
 
     #[derive(Debug)]
-    struct ComponentStore<C: Component>(PhantomData<C>);
+    struct TestStore<C: Component>(PhantomData<C>);
+    impl<C: Component> ComponentStore for TestStore<C> {}
 
     #[derive(Debug)]
     struct C;
 
     #[rustfmt::skip]
-    impl<C: Component> Store for ComponentStore<C> {
+    impl<C: Component> Store for TestStore<C> {
         type Item = C;
 
-        fn new() -> Self { ComponentStore(PhantomData) }
+        fn new() -> Self { TestStore(PhantomData) }
         fn push(&mut self, _: Self::Item) -> usize { 0 }
      }
 
     #[rustfmt::skip]
-    impl<C: Component> Default for ComponentStore<C> {
+    impl<C: Component> Default for TestStore<C> {
         fn default() -> Self { Self::new() }
     }
 
     #[test]
     fn test_implicit_component_trait() {
-        let mut cs = ComponentStore::<C>::new();
+        let mut cs = TestStore::<C>::new();
 
         assert_eq!(cs.push(C), 0);
     }
