4878d9c4e8ea697d70bd142a14b5033a14d16449
Jean Mertz <jean@mertz.fm>
2018-12-19 16:11:36 +0100

    feat(component): introduce ComponentCollection trait
    This trait will be used to allow creating a single entity with multiple
    components from different types in one go. This functionality does not
    exist yet, but this is the start to such an implementation.

--- src/component.rs
+++ src/component.rs
@@ -8,6 +8,13 @@ pub trait Component: 'static + Debug {}
 /// static lifetime, and implements `Debug`.
 impl<T> Component for T where T: 'static + Debug {}
 
+/// A collection of one or more components should implement this trait to be
+/// able to manipulate each individual component, or store them for later use.
+pub trait ComponentCollection {}
+
+impl<A: Component> ComponentCollection for (A,) {}
+impl<A: Component, B: Component> ComponentCollection for (A, B) {}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -18,5 +25,24 @@ mod tests {
         struct C;
 
         let _: Box<Component> = Box::new(C);
+    }
+
+    #[test]
+    fn test_component_collection() {
+        #[derive(Debug)]
+        struct C;
+
+        let _: Box<ComponentCollection> = Box::new((C, C));
+    }
+
+    #[test]
+    fn test_component_collection_heterogeneous() {
+        #[derive(Debug)]
+        struct C;
+
+        #[derive(Debug)]
+        struct D(usize);
+
+        let _: Box<ComponentCollection> = Box::new((C, D(1)));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,5 @@
 mod component;
 mod store;
 
+use crate::component::ComponentCollection;
 pub use crate::{component::Component, store::Store};
