0e3ac24b78162d50ccb6af4313dde3a34ebe0e5c
Jean Mertz <jean@mertz.fm>
2019-01-27 15:54:30 +0100

    test(system): add read/write system integration test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,4 +1,4 @@
-use things::{Query, Read, System, Things};
+use things::{Query, Read, System, Things, Write};
 
 struct AssertValues;
 impl<'a> System<'a> for AssertValues {
@@ -17,4 +17,24 @@ fn test_reader_system() {
     let mut ecs = Things::new();
     ecs.create_entity((10, "hello"));
     ecs.execute_system::<AssertValues>();
+}
+
+struct IncrementCounter;
+impl<'a> System<'a> for IncrementCounter {
+    type Query = (Read<&'static str>, Write<i32>);
+
+    fn update(components: <Self::Query as Query<'a>>::Iter) {
+        for (string, int) in components {
+            *int += 1;
+            assert_eq!(int, &11);
+            assert_eq!(string, &"hello");
+        }
+    }
+}
+
+#[test]
+fn test_writer_system() {
+    let mut ecs = Things::new();
+    ecs.create_entity(("hello", 10));
+    ecs.execute_system::<IncrementCounter>();
 }
