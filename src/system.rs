6040f8774c6a12d19f2805cc7282f6714a3de62e
Jean Mertz <jean@mertz.fm>
2019-01-27 15:43:08 +0100

    feat(system): allow acting on a (Read, Write) tuple
    This is the first implementation that allows accessing both immutable
    and mutable components by a single system.
    
    Right now, this implementation is still unsound, as you can access
    multiple mutable components across different systems.
    
    See commit 6161ce5 for more details on how and when this soundness hole
    will be filled.

--- src/system.rs
+++ src/system.rs
@@ -7,10 +7,6 @@ use rustc_hash::FxHashMap as HashMap;
 /// System must be implemented by any object that wants to interact with
 /// components. Objects implementing `System` are used to manipulate, and act on
 /// the data stored by components.
-///
-/// The hierarchy of a system is as follows:
-///
-/// System -> Query -> Join<Read/Write<Component>, StatementN...>
 pub trait System<'a> {
     type Query: Query<'a>;
 
@@ -25,11 +21,22 @@ pub trait Query<'a> {
     fn iter(store: &'a HashMap<TypeId, Box<ComponentStore>>) -> Self::Iter;
 }
 
-/// A `Statement` defines a single `Read` or `Write` action for a `Component`
-/// type. It is meant to return an iterator for that type.
-pub trait Statement<'a> {
+/// A `Reader` defines a single `Read` action for a component type. Its `find`
+/// operation returns an iterator over all immutable references of the given
+/// [`Component`].
+pub trait Reader<'a> {
     type Component: Component;
     type Iter: ExactSizeIterator<Item = &'a Option<Self::Component>>;
+
+    fn find(store: &'a HashMap<TypeId, Box<ComponentStore>>) -> Self::Iter;
+}
+
+/// A `Writer` defines a single `Write` action for a component type. Its `find`
+/// operation returns an iterator over all mutable references of the given
+/// [`Component`].
+pub trait Writer<'a> {
+    type Component: Component;
+    type Iter: ExactSizeIterator<Item = &'a mut Option<Self::Component>>;
 
     fn find(store: &'a HashMap<TypeId, Box<ComponentStore>>) -> Self::Iter;
 }
@@ -40,40 +47,76 @@ pub struct Read<C: Component>(C);
 /// `Write` marks a `Component` within a `Query` as read-and-write.
 pub struct Write<C: Component>(C);
 
-impl<'a, C: Component> Statement<'a> for Read<C> {
+impl<'a, C: Component> Reader<'a> for Read<C> {
     type Component = C;
     type Iter = std::slice::Iter<'a, Option<Self::Component>>;
 
     fn find(store: &'a HashMap<TypeId, Box<ComponentStore>>) -> Self::Iter {
         let id = TypeId::of::<C>();
-        if let Some(store) = store.get(&id) {
-            return store.as_store::<C>().unwrap().as_slice().iter();
+        match store.get(&id) {
+            Some(store) => store.as_store::<C>().unwrap().as_slice().iter(),
+            None => (&[]).iter(),
         }
+    }
+}
 
-        (&[]).iter()
+// FIXME: This implementation requires a runtime borrow-checker to make sure
+//        only one mutable reference can be fetched to any given [`Component`]
+//        at any given time.
+impl<'a, C: Component> Writer<'a> for Write<C> {
+    type Component = C;
+    type Iter = std::slice::IterMut<'a, Option<Self::Component>>;
+
+    fn find(store: &'a HashMap<TypeId, Box<ComponentStore>>) -> Self::Iter {
+        let id = TypeId::of::<C>();
+        match store.get(&id) {
+            Some(store) => unsafe { store.as_store::<C>().unwrap().as_mut_slice() }.iter_mut(),
+            None => (&mut []).iter_mut(),
+        }
     }
 }
 
 // TODO: This is a temporary stub implementation for a `Query` consisting of a
-//       `Join` of two `Statement`s. In the future, this will be implemented for
-//       A..Z using a macro.
-impl<'a, A, B> Query<'a> for (A, B)
+//       `Read`, and a `Write`. In the future, this will be implemented for A..Z
+//       using a macro.
+impl<'a, A, B> Query<'a> for (Read<A>, Write<B>)
 where
-    A: Statement<'a>,
-    B: Statement<'a>,
+    A: Component,
+    B: Component,
 {
-    // TODO: simplify type signature
-    #[allow(clippy::type_complexity)]
     type Iter = FilterMap<
-        Zip<A::Iter, B::Iter>,
-        fn(
-            (&'a Option<A::Component>, &'a Option<B::Component>),
-        ) -> Option<(&'a A::Component, &'a B::Component)>,
+        Zip<<Read<A> as Reader<'a>>::Iter, <Write<B> as Writer<'a>>::Iter>,
+        fn((&'a Option<A>, &'a mut Option<B>)) -> Option<(&'a A, &'a mut B)>,
     >;
 
     fn iter(store: &'a HashMap<TypeId, Box<ComponentStore>>) -> Self::Iter {
-        let a = A::find(store);
-        let b = B::find(store);
+        let a = <Read<A> as Reader<'a>>::find(store);
+        let b = <Write<B> as Writer<'a>>::find(store);
+
+        a.zip(b)
+            .filter_map(|(x, y)| match (x.as_ref(), y.as_mut()) {
+                (Some(x), Some(y)) => Some((x, y)),
+                _ => None,
+            })
+    }
+}
+
+// TODO: This is a temporary stub implementation for a `Query` consisting of two
+//       `Read`s. In the future, this will be implemented for A..Z using a
+//       macro.
+impl<'a, A, B> Query<'a> for (Read<A>, Read<B>)
+where
+    A: Component,
+    B: Component,
+{
+    type Iter = FilterMap<
+        Zip<<Read<A> as Reader<'a>>::Iter, <Read<B> as Reader<'a>>::Iter>,
+        fn((&'a Option<A>, &'a Option<B>)) -> Option<(&'a A, &'a B)>,
+    >;
+
+    fn iter(store: &'a HashMap<TypeId, Box<ComponentStore>>) -> Self::Iter {
+        let a = <Read<A> as Reader<'a>>::find(store);
+        let b = <Read<B> as Reader<'a>>::find(store);
 
         a.zip(b)
             .filter_map(|(a, b)| match (a.as_ref(), b.as_ref()) {
